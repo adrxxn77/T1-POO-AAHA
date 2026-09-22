44e591c9a841aba8c0fbe3047c9ae20df1f5e8d7
adrxxn77 <N00426922@upn.pe>
2026-09-21 20:44:56 -0500

    sexto commit AAHA

--- src/institutoinnova/AlumnoControlador.java
+++ src/institutoinnova/AlumnoControlador.java
@@ -4,10 +4,31 @@
  */
 package institutoinnova;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author herre
  */
 public class AlumnoControlador {
+    
+    private ArrayList<Alumno> listaAlumnos = new ArrayList<>();
+    
+    public void agregarAlumno(Alumno alumno) {
+        listaAlumnos.add(alumno);
+    }
+    
+    public void listarAlumnos() {
+        
+        for (int i = 0; i < listaAlumnos.size(); i++) {
+            
+            System.out.println("Nombre: " + listaAlumnos.get(i).getNombre());
+            System.out.println("Tipo de documento: " + listaAlumnos.get(i).getTipoDocumento());
+            System.out.println("Numero de documento: " + listaAlumnos.get(i).getNumeroDocumento());
+            System.out.println("Nivel socioeconomico: " + listaAlumnos.get(i).getNivelSocioeconomico());
+            System.out.println("Tipo de beca: " + listaAlumnos.get(i).getTipoBeca());
+            System.out.println("-----------------------------");
+        }
+    }
     
 }
