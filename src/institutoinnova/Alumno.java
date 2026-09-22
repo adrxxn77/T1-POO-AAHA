140c57b463e2239213c91993789f9eacaa708b76
adrxxn77 <N00426922@upn.pe>
2026-09-21 20:25:10 -0500

    cuarto commit AAHA

--- src/institutoinnova/Alumno.java
+++ src/institutoinnova/Alumno.java
@@ -10,4 +10,15 @@ package institutoinnova;
  */
 public class Alumno {
     
+    private String nombre;
+    private String tipoDocumento;
+    private String numeroDocumento;
+    private String nivelSocioeconomico;
+    private String tipoBeca;
+
+    public Alumno() {
+        
+    }
+    
+    
 }
